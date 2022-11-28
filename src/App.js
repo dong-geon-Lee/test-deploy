87b619a2dbdd0006659f2f4482fd31f770521994
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-11-28 19:24:23 +0900

    왜 페이지 안뜨지??

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
-import AddCounter from "./components/AddCounter";
 
 const App = () => {
   return (
     <div className="App">
       <h1>Hello World! 이제 성공함!</h1>
       <h2>How to you?</h2>
-      <AddCounter />
     </div>
   );
 };
